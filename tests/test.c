e3b04a189ee43317dfca0959cd2324647eda5ca1
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-21 12:09:36 +0530

    c test

--- tests/test.c
+++ tests/test.c
@@ -1,37 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
 
-// Definition for singly-linked list node
-struct Node {
-    int data;
-    struct Node* next;
-};
-
-// Function to create a new node
-struct Node* createNode(int data) {
-    // Function body here
-}
-
-// Function to create a linked list from an array of integers
-struct Node* createLinkedList(int arr[], int n) {
-    // Function body here
-}
-
-// Function to get the count of nodes
-int getCount(struct Node* head) {
-    // Function body here
-}
+// Declare functions from solution.c
+extern struct Node* createNode(int data);
+extern struct Node* createLinkedList(int arr[], int n);
+extern int getCount(struct Node* head);
 
 // Function to run each test case
 bool runTest(int arr[], int n, int expected) {
-    // Test logic here
+    struct Node* head = createLinkedList(arr, n);
+    int result = getCount(head);
+    return result == expected;
 }
 
 int main() {
     bool allPassed = true;
 
-    // Test cases (no logic here, just test input and expected output)
+    // Test cases
     allPassed &= runTest((int[]){1, 2, 3, 4, 5}, 5, 5);
     allPassed &= runTest((int[]){2, 4, 6, 7, 5, 1, 0}, 7, 7);
     allPassed &= runTest((int[]){10}, 1, 1);

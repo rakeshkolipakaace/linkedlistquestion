9e8dc7659f822edcbc64f6dc546b1e52475a1516
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-21 15:47:01 +0530

    py

--- solutions/solution.py
+++ solutions/solution.py
@@ -5,22 +5,5 @@ class Node:
 
 class Solution:
     def getCount(self, head):
-        count = 0
-        current = head
-        while current:
-            count += 1
-            current = current.next
-        return count
+#  write the code here 
 
-    def create_linked_list(self, arr):
-        if not arr:
-            return None
-        head = Node(arr[0])
-        current = head
-        for value in arr[1:]:
-            current.next = Node(value)
-            current = current.next
-        return head
-
-
-# code here 

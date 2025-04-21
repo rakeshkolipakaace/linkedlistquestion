8e04d70b0701eda71bdd8c91ab2d178809477c46
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-21 12:14:25 +0530

    c sol11

--- solutions/solution.c
+++ solutions/solution.c
@@ -1,7 +1,5 @@
-//{ Driver Code Starts
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 // Definition for singly-linked list node
 struct Node {
@@ -9,23 +7,9 @@ struct Node {
     struct Node* next;
 };
 
-
-// } Driver Code Ends
-
-// Solution code: function to get the count of nodes
-int getCount(struct Node* head) {
-    // write code here
-}
-
-
-//{ Driver Code Starts.
-//  Function to create a new node
+// Function to create a new node
 struct Node* createNode(int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    if (newNode == NULL) { // Check for memory allocation failure
-        printf("Memory allocation failed\n");
-        exit(1);
-    }
     newNode->data = data;
     newNode->next = NULL;
     return newNode;
@@ -33,74 +17,22 @@ struct Node* createNode(int data) {
 
 // Function to create a linked list from an array of integers
 struct Node* createLinkedList(int arr[], int n) {
-    if (n == 0)
-        return NULL;
-
     struct Node* head = createNode(arr[0]);
-    struct Node* tail = head;
-
+    struct Node* temp = head;
     for (int i = 1; i < n; i++) {
-        tail->next = createNode(arr[i]);
-        tail = tail->next;
+        temp->next = createNode(arr[i]);
+        temp = temp->next;
     }
-
     return head;
 }
 
-// Trim trailing newline from fgets input
-void trimNewline(char* str) {
-    size_t length = strlen(str);
-    if (length > 0 && str[length - 1] == '\n') {
-        str[length - 1] = '\0';
+// Function to get the count of nodes
+int getCount(struct Node* head) {
+    int count = 0;
+    struct Node* current = head;
+    while (current != NULL) {
+        count++;
+        current = current->next;
     }
+    return count;
 }
-
-int main() {
-    int t;
-    scanf("%d\n", &t); // Read number of test cases
-
-    while (t--) {
-        size_t buffer_size = 1000000; // Allocate a large buffer size for input
-        char* input = (char*)malloc(buffer_size * sizeof(char));
-
-        if (input == NULL) { // Check if memory allocation for input fails
-            printf("Memory allocation failed\n");
-            exit(1);
-        }
-
-        fgets(input, buffer_size, stdin); // Read input as a string
-
-        // Trim any trailing newline from the input
-        trimNewline(input);
-
-        // Parse the input string into an array of integers
-        int arr[100000], n = 0; // Increased size to handle large inputs
-        char* token = strtok(input, " ");
-        while (token != NULL) {
-            arr[n++] = atoi(token);
-            token = strtok(NULL, " ");
-        }
-
-        // Create the linked list from the array
-        struct Node* head = createLinkedList(arr, n);
-
-        // Call the getCount function and print the result
-        printf("%d\n", getCount(head));
-
-        // Free the allocated memory for the linked list
-        struct Node* temp;
-        while (head != NULL) {
-            temp = head;
-            head = head->next;
-            free(temp);
-        }
-
-        // Free the allocated memory for input string
-        free(input);
-        printf("~\n");
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends

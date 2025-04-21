e8168b68e8c72340de0e413a39a405a5961ac0d8
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-21 14:47:53 +0530

    c solution 8

--- solutions/solution.c
+++ solutions/solution.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
 struct Node {
     int data;
     struct Node* next;
 };
-
-
 
 int getCount(struct Node* head) {
     int count = 0;
@@ -16,15 +15,11 @@ int getCount(struct Node* head) {
         head = head->next;
     }
     return count;
-    // write code here
 }
-
-
 
 struct Node* createNode(int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     if (newNode == NULL) { 
-        printf("Memory allocation failed\n");
         exit(1);
     }
     newNode->data = data;
@@ -33,8 +28,7 @@ struct Node* createNode(int data) {
 }
 
 struct Node* createLinkedList(int arr[], int n) {
-    if (n == 0)
-        return NULL;
+    if (n == 0) return NULL;
 
     struct Node* head = createNode(arr[0]);
     struct Node* tail = head;
@@ -46,51 +40,3 @@ struct Node* createLinkedList(int arr[], int n) {
 
     return head;
 }
-
-void trimNewline(char* str) {
-    size_t length = strlen(str);
-    if (length > 0 && str[length - 1] == '\n') {
-        str[length - 1] = '\0';
-    }
-}
-
-int main() {
-    int t;
-    scanf("%d\n", &t); 
-    while (t--) {
-        size_t buffer_size = 1000000; 
-        char* input = (char*)malloc(buffer_size * sizeof(char));
-
-        if (input == NULL) { 
-            printf("Memory allocation failed\n");
-            exit(1);
-        }
-
-        fgets(input, buffer_size, stdin); 
-        trimNewline(input);
-
-        int arr[100000], n = 0; 
-        char* token = strtok(input, " ");
-        while (token != NULL) {
-            arr[n++] = atoi(token);
-            token = strtok(NULL, " ");
-        }
-
-        struct Node* head = createLinkedList(arr, n);
-
-        printf("%d\n", getCount(head));
-
-        struct Node* temp;
-        while (head != NULL) {
-            temp = head;
-            head = head->next;
-            free(temp);
-        }
-
-        free(input);
-        printf("~\n");
-    }
-
-    return 0;
-}
-

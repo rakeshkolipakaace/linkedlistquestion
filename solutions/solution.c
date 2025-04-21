be2cc492c888fdad09d7424743b695e82dd2ca95
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-21 12:57:39 +0530

    c ans12345

--- solutions/solution.c
+++ solutions/solution.c
@@ -1,4 +1,38 @@
-// Solution code: function to get the count of nodes
+#include <stddef.h>  // for NULL
+#include <stdlib.h>  // for malloc
+
+// Definition for singly-linked list node..
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to create a linked list from an array
+struct Node* createLinkedList(int arr[], int n) {
+    struct Node* head = NULL;
+    struct Node* temp = NULL;
+
+    for (int i = 0; i < n; i++) {
+        struct Node* newNode = createNode(arr[i]);
+        if (head == NULL) {
+            head = newNode;
+        } else {
+            temp->next = newNode;
+        }
+        temp = newNode;
+    }
+    return head;
+}
+
+// Function to get the count of nodes
 int getCount(struct Node* head) {
     int count = 0;
     while (head != NULL) {

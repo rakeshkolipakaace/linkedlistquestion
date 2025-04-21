add5e7d4e7b9a6d6457db632e35562abce45fe5c
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-21 12:10:04 +0530

    c sol

--- solutions/solution.c
+++ solutions/solution.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
+// Definition for singly-linked list node
 struct Node {
     int data;
     struct Node* next;
 };
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to create a linked list from an array of integers
+struct Node* createLinkedList(int arr[], int n) {
+    struct Node* head = createNode(arr[0]);
+    struct Node* temp = head;
+    for (int i = 1; i < n; i++) {
+        temp->next = createNode(arr[i]);
+        temp = temp->next;
+    }
+    return head;
+}
 
 // Function to get the count of nodes
 int getCount(struct Node* head) {
@@ -16,32 +35,4 @@ int getCount(struct Node* head) {
         current = current->next;
     }
     return count;
-}
-
-// Function to create a new node
-struct Node* createNode(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    if (newNode == NULL) { // Check for memory allocation failure
-        printf("Memory allocation failed\n");
-        exit(1);
-    }
-    newNode->data = data;
-    newNode->next = NULL;
-    return newNode;
-}
-
-// Function to create a linked list from an array of integers
-struct Node* createLinkedList(int arr[], int n) {
-    if (n == 0)
-        return NULL;
-
-    struct Node* head = createNode(arr[0]);
-    struct Node* tail = head;
-
-    for (int i = 1; i < n; i++) {
-        tail->next = createNode(arr[i]);
-        tail = tail->next;
-    }
-
-    return head;
 }

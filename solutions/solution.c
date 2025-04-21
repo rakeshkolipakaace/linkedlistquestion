f1281b9cb8ea18454099d03a4d8ca3a0fea4b6fa
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-21 12:17:57 +0530

    c sol1111

--- solutions/solution.c
+++ solutions/solution.c
@@ -1,3 +1,13 @@
+#include <stddef.h>  // for NULL
+#include <stdlib.h>  // for malloc
+
+// Definition for singly-linked list node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to get the count of nodes
 int getCount(struct Node* head) {
     int count = 0;
     while (head != NULL) {

e4c5c8ed22efda172f2671a464045938ba7ee75a
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-21 15:01:27 +0530

    c++ solution

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -28,6 +28,15 @@ class Solution {
   public:
     // Function to count nodes of a linked list.
     int getCount(struct Node* head) {
+        int count = 0;
+        Node* current = head;
+
+        while (current != NULL) {
+            count++;
+            current = current->next;
+        }
+
+        return count;
 
         // Code here
     }

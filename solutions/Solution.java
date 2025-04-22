c7e2051e5612a98a574b1cd759eb4d9d9c26cb9f
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-22 11:53:11 +0530

    java base code

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -9,28 +9,18 @@ class Node {
 }
 
 class Solution {
-    // Function to count nodes of a linked list.
     public int getCount(Node head) {
-        int count = 0; // Initialize count to 0
-        Node current = head; // Start from the head node
-        
-        // Traverse the linked list
-        while (current != null) {
-            count++; // Increment count for each node
-            current = current.next; //
-        }
-        
-        return count; 
+
+        return 0;
+       
     }
 
-    // Function to create linked list from an array
     public Node createLinkedList(int[] input) {
         if (input.length == 0) return null;
         
         Node head = new Node(input[0]);
         Node current = head;
         
-        // Create the rest of the linked list
         for (int i = 1; i < input.length; i++) {
             current.next = new Node(input[i]);
             current = current.next;

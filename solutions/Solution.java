25c21080dc5eda8bf86daa9c2f110c62d1345ab3
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-22 12:23:01 +0530

    java test cases

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -15,7 +15,7 @@ class Solution {
         
         // Traverse the linked list
         while (current != null) {
-            count++; 
+            count++; // Increment count for each node
             current = current.next;
         }
         

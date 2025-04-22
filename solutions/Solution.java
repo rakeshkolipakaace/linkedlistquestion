fb6d6829b1713ed28f4f4912413e0f4d3f67e766
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-22 11:43:25 +0530

    java cases

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -1,7 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
 class Node {
     int data;
     Node next;
@@ -12,21 +8,10 @@ class Node {
     }
 }
 
-
-// } Driver Code Ends
-
-/*Complete the function below*/
-/*
-class Node{
-    int data;
-    Node next;
-    Node(int a){  data = a; next = null; }
-}*/
-
 class Solution {
     // Function to count nodes of a linked list.
     public int getCount(Node head) {
-         int count = 0; // Initialize count to 0
+        int count = 0; // Initialize count to 0
         Node current = head; // Start from the head node
         
         // Traverse the linked list
@@ -36,42 +21,21 @@ class Solution {
         }
         
         return count; 
-        // code here
     }
 
+    // Function to create linked list from an array
     public Node createLinkedList(int[] input) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createLinkedList'");
-    }
-}
-
-
-//{ Driver Code Starts.
-
-class LinkedList {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-            ArrayList<Integer> arr = new ArrayList<>();
-            String input = br.readLine();
-            StringTokenizer st = new StringTokenizer(input);
-            while (st.hasMoreTokens()) {
-                arr.add(Integer.parseInt(st.nextToken()));
-            }
-            Node head = new Node(arr.get(0));
-            Node tail = head;
-            for (int i = 1; i < arr.size(); ++i) {
-                tail.next = new Node(arr.get(i));
-                tail = tail.next;
-            }
-            Solution ob = new Solution();
-            System.out.println(ob.getCount(head));
+        if (input.length == 0) return null;
         
-System.out.println("~");
-}
+        Node head = new Node(input[0]);
+        Node current = head;
+        
+        // Create the rest of the linked list
+        for (int i = 1; i < input.length; i++) {
+            current.next = new Node(input[i]);
+            current = current.next;
+        }
+        
+        return head;
     }
 }
-
-// } Driver Code Ends
--- tests/Test.java
+++ tests/Test.java
@@ -1,6 +1,6 @@
-import java.util.*;
+import java.util.Arrays;
 
-public class Test {
+class Test {
     public static void main(String[] args) {
         Solution sol = new Solution();
         boolean allPassed = true;
